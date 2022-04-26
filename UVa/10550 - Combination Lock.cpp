84ae213163d18e42072bc52624698c484fca5a60
Shantanu <shantanuk@tutanota.com>
2022-04-26 13:30:36 -0400

    20220426

---
+++ UVa/10550 - Combination Lock.cpp
@@ -0,0 +1,15 @@
+// uVa 10550 - Combination Lock
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int start, first, second, third;
+    while(1){
+        cin >> start >> first >> second >> third;
+        if(!start && !first && !second && !third) break;
+        printf("%d\n", 1080 + ((start - first + 40) % 40 + (second - first + 40) % 40 + (second - third + 40) % 40) * 9);
+    }
+
+    return 0;
+}

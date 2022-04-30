99bf56069636454a82def07c10b2ae6abcae5804
Shantanu <shantanuk@tutanota.com>
2022-04-30 12:44:12 -0400

    20220430

---
+++ LeetCode/54. Spiral Matrix.cpp
@@ -0,0 +1,44 @@
+// LeetCode 54. Spiral Matrix
+// Simulation.
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix){
+        int m = matrix.size(), n = matrix[0].size(), i = 0, j = 0, big = 9999;
+        vector<int> vr;
+        while(1){
+            // Go right.
+            while(j < n && matrix[i][j] != big){
+                vr.push_back(matrix[i][j]);
+                matrix[i][j] = big;
+                j++;
+            }
+            i++, j--;
+            if(vr.size() == m * n) return vr;
+            // Go down.
+            while(i < m && matrix[i][j] != big){
+                vr.push_back(matrix[i][j]);
+                matrix[i][j] = big;
+                i++;
+            }
+            i--, j--;
+            if(vr.size() == m * n) return vr;
+            // Go left.
+            while(j >= 0 && matrix[i][j] != big){
+                vr.push_back(matrix[i][j]);
+                matrix[i][j] = big;
+                j--;
+            }
+            i--, j++;
+            if(vr.size() == m * n) return vr;
+            // Go up.
+            while(i >= 0 && matrix[i][j] != big){
+                vr.push_back(matrix[i][j]);
+                matrix[i][j] = big;
+                i--;
+            }
+            i++, j++;
+            if(vr.size() == m * n) return vr;
+        }
+        return vr;
+    }
+};

2a44b6283cc4a43fed092fb8bc4927b38ecae11d
shantanuk09 <shantanuk@tutanota.com>
2022-11-02 07:52:21 -0400

    20221102

--- LeetCode/977. Squares of a Sorted Array.py
+++ LeetCode/977. Squares of a Sorted Array.py
@@ -1,12 +1,12 @@
 # LeetCode 977. Squares of a Sorted Array
+# Use a map.
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        arr, square = [0] * 10005, []
-        for e in nums : 
-            if e < 0 : e = -e
-            arr[e] += 1
-        for i in range(0, 10005) : 
-            if arr[i] > 0 : 
-                for j in range(0, arr[i]) : 
-                    square.append(i * i)
-        return square
+        mapping, arr = [0] * 10005, []
+        for n in nums : 
+            mapping[abs(n)] += 1
+        for i in range(0, len(mapping)) : 
+            if mapping[i] > 0 : 
+                for j in range(0, mapping[i]) : 
+                    arr.append(i ** 2)
+        return arr

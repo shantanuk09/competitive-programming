db6c604d2d140459980e612e91b2e1af3ec99c3b
shantanuk09 <shantanuk@tutanota.com>
2022-11-05 20:49:09 -0400

    20221105

---
+++ LeetCode/19. Remove Nth Node From End of List .cpp
@@ -0,0 +1,15 @@
+// LeetCode 19. Remove Nth Node From End of List
+// Slow and fast pointers
+class Solution{
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n){
+        ListNode *slow = head, *fast = head;
+        for(int i = 0; i < n; i++)
+            fast = fast -> next;
+        if(!fast) return head -> next;
+        while(fast -> next)
+            slow = slow -> next, fast = fast -> next;
+        slow -> next = slow -> next -> next;
+        return head;
+    }
+};

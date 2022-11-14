e7a98c3dfbe04b908facfc2b45f01d6cdc1c1eb3
shantanuk09 <shantanuk@tutanota.com>
2022-11-14 10:38:27 -0500

    20221114

--- LeetCode/136. Single Number.py
+++ LeetCode/136. Single Number.py
@@ -1,7 +1,8 @@
 # LeetCode 136. Single Number
+# XOR Trick.
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        xor = 0
+        answer = 0
         for e in nums : 
-            xor ^= e
-        return xor
+            answer ^= e
+        return answer

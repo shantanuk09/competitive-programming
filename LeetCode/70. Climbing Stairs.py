89b4ad2f8087e396c32a545e49e51c18629c506a
shantanuk09 <shantanuk@tutanota.com>
2022-11-18 12:31:27 -0500

    20221118

--- LeetCode/70. Climbing Stairs.py
+++ LeetCode/70. Climbing Stairs.py
@@ -1,19 +1,8 @@
 # LeetCode 70. Climbing Stairs
-#Dynamic Programming
+# Dynamic Programming. 
 class Solution:
     def climbStairs(self, n: int) -> int:
         arr = [0, 1, 2]
         for i in range(3, n + 1) : 
             arr.append(arr[-1] + arr[-2])
-        return arr[n]
-
-# Return the n-th Fibonacci number.
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        if n == 1 : return 1
-        first, second = 1, 2
-        for i in range(3, n + 1) : 
-            third = first + second
-            first = second
-            second = third
-        return second
+        return arr[n] 

951d4a01807a5037113e21dec985cf1baaab9eda
shantanuk09 <shantanuk@tutanota.com>
2022-11-19 19:50:58 -0500

    20221119

--- LeetCode/139. Word Break.cpp
+++ LeetCode/139. Word Break.cpp
@@ -1,14 +1,14 @@
 // LeetCode 139. Word Break
-// Dynamic Programming
+// Dynamic Programming | Time Complexity O(n^3) | Space Complexity O(n)
 class Solution{
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
+    bool wordBreak(string s, vector<string>& wordDict){
         set<string> st(wordDict.begin(), wordDict.end());
         vector<bool> vr(s.size() + 1);
         vr[0] = true;
         for(int i = 1; i <= s.size(); i++){
             for(int j = 0; j < i; j++){
-                if(vr[j] and st.find(s.substr(j, i - j)) != st.end()){
+                if(vr[j] && st.find(s.substr(j, i - j)) != st.end()){
                     vr[i] = true;
                     break;
                 }

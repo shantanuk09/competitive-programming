60cbcf70b1a3a732687229f02fd76048dccf764a
shantanuk09 <shantanuk@tutanota.com>
2022-11-20 10:08:41 -0500

    20221120

--- LeetCode/300. Longest Increasing Subsequence.cpp
+++ LeetCode/300. Longest Increasing Subsequence.cpp
@@ -1,14 +1,13 @@
 // LeetCode 300. Longest Increasing Subsequence
-// Dynamic Programming
-class Solution {
+//Dynamic Programming.
+class Solution{
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size(), longest = 1;
-        int arr[n];
+    int lengthOfLIS(vector<int>& nums){
+        int n = nums.size(), arr[n], longest = 1;
         fill(arr, arr + n, 1);
         for(int i = 1; i < n; i++){
             for(int j = 0; j < i; j++){
-                if(nums[i] > nums[j]) arr[i] = max(arr[i], arr[j] + 1);
+                if(nums[j] < nums[i]) arr[i] = max(arr[i], 1 + arr[j]);
                 longest = max(longest, arr[i]);
             }
         }

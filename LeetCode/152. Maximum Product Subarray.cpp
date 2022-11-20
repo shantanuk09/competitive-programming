ad4afef421cac54eb2932868eec3bbf1828f38a1
shantanuk09 <shantanuk@tutanota.com>
2022-11-19 19:33:07 -0500

    20221119

--- LeetCode/152. Maximum Product Subarray.cpp
+++ LeetCode/152. Maximum Product Subarray.cpp
@@ -1,12 +1,12 @@
 // LeetCode 152. Maximum Product Subarray
-class Solution {
+// Dynamic Programming.
+class Solution{
 public:
-    int maxProduct(vector<int>& nums) {
-        int minProduct = 1, maxProduct = 1, largest = INT_MIN;
-        for(int e : nums){
+    int maxProduct(vector<int>& nums){
+        int maxProduct = 1, minProduct = 1, largest = INT_MIN;
+        for(auto e : nums){
             if(e < 0) swap(maxProduct, minProduct);
-            maxProduct = max(e, maxProduct * e);
-            minProduct = min(e, minProduct * e);
+            maxProduct = max(maxProduct * e, e), minProduct = min(minProduct * e, e);
             largest = max(largest, maxProduct);
         }
         return largest;

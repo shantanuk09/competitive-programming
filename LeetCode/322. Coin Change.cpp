d29df5218ba8bd629af34c06738883a760d33e16
shantanuk09 <shantanuk@tutanota.com>
2022-11-19 19:22:29 -0500

    20221119

--- LeetCode/322. Coin Change.cpp
+++ LeetCode/322. Coin Change.cpp
@@ -1,18 +1,18 @@
 // LeetCode 322. Coin Change
 // Dynamic Programming
-class Solution {
+class Solution{
 public:
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange(vector<int>& coins, int amount){
+        int arr[amount + 1];
+        fill(arr, arr + amount + 1, INT_MAX);
         sort(coins.begin(), coins.end());
-        int arr[++amount];
         arr[0] = 0;
-        for(int i = 1; i < amount; i++){
-            arr[i] = INT_MAX;
-            for(int coin : coins){
-                if(i - coin < 0) break;
-                if(arr[i - coin] != INT_MAX) arr[i] = min(arr[i - coin] + 1, arr[i]);
+        for(int i = 1; i <= amount; i++){
+            for(auto coin : coins){
+                if(i < coin) break;
+                if(arr[i - coin] != INT_MAX) arr[i] = min(arr[i], arr[i - coin] + 1);
             }
         }
-        return (arr[--amount] != INT_MAX) ? (arr[amount]) : -1;
+        return arr[amount] != INT_MAX ? arr[amount] : -1;
     }
 };

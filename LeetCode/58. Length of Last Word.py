5d26959e099e14f15e9a22529d25961c42a2affc
shantanuk09 <shantanuk@tutanota.com>
2022-11-26 10:40:52 -0500

    20221126

---
+++ LeetCode/58. Length of Last Word.py
@@ -0,0 +1,8 @@
+# LeetCode 58. Length of Last Word
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        length = 0
+        for i in range(len(s) - 1, -1, -1) : 
+            if s[i].isalpha() : length += 1
+            elif length > 0 and s[i] == " " : break
+        return length

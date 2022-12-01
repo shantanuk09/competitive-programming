f03ca61b902cc5976711a83544b4fcf1975e97ca
shantanuk09 <shantanuk@tutanota.com>
2022-12-01 11:31:34 -0500

    20221201

---
+++ LeetCode/45. Jump Game II.cpp
@@ -0,0 +1,20 @@
+// 45. Jump Game II
+// Greedy.
+class Solution{
+public:
+    int jump(vector<int>& nums){
+        int i = 0;
+        int n = nums.size();
+        int jumps = 0;
+        int currentPosition = 0;
+        int maxReachable = 0;
+        while(currentPosition < n - 1){
+            maxReachable = max(maxReachable, i + nums[i]);
+            if(i == currentPosition) currentPosition = maxReachable, jumps++;
+            i++;
+        }
+        return jumps;
+    }
+};
+
+

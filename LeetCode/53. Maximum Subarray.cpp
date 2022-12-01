68f8a0aa613141fdbd2daea0a1693a9079dd39c5
shantanuk09 <shantanuk@tutanota.com>
2022-12-01 10:19:56 -0500

    20221201

--- LeetCode/53. Maximum Subarray.cpp
+++ LeetCode/53. Maximum Subarray.cpp
@@ -1,14 +1,14 @@
 // LeetCode 53. Maximum Subarray
-// Dynamic Programming.
-class Solution {
+// Dynamic Programming 
+class Solution{
 public:
-    int maxSubArray(vector<int>& nums) {
-        int maxSum = nums[0], arr[100000] = {nums[0]};
+    int maxSubArray(vector<int>& nums){
+        int big = nums[0];
+        int arr[100005] = {nums[0]};
         for(int i = 1; i < nums.size(); i++){
-            arr[i] = max(nums[i], nums[i] + arr[i - 1]);
-            maxSum = max(maxSum, arr[i]);
+            arr[i] = max(arr[i - 1] + nums[i], nums[i]);
+            big = max(big, arr[i]);
         }
-        return maxSum;
+        return big;
     }
 };
-

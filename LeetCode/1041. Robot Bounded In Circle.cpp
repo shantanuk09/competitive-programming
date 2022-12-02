d7171795adfbadb6784efe0b2670365cb7587513
shantanuk09 <shantanuk@tutanota.com>
2022-12-02 09:33:03 -0500

    20221202

---
+++ LeetCode/1041. Robot Bounded In Circle.cpp
@@ -0,0 +1,16 @@
+// 45. 1041. Robot Bounded In Circle
+// Mathematics.
+class Solution{
+public:
+    bool isRobotBounded(string instructions){
+        int x = 0, y = 0;
+        int index = 0;
+        int directions[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        for(char c  : instructions){
+            if(c == 'L') index = (index + 3) % 4;
+            else if(c == 'R') index = (index + 1) % 4;
+            else x += directions[index][0], y += directions[index][1];
+        }
+        return ((x == 0 && y == 0) || index != 0); 
+    }
+};

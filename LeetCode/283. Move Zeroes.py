e5ff8fa303e9326a0b6750a1b81f9c47f7c3f553
shantanuk09 <shantanuk@tutanota.com>
2022-12-10 11:23:00 -0500

    20221210

--- LeetCode/283. Move Zeroes.py
+++ LeetCode/283. Move Zeroes.py
@@ -1,19 +1,9 @@
 # LeetCode 283. Move Zeroes
+# Two pointer.
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        zero = nums.count(0)
-        while nums.count(0) : 
-            nums.remove(0)
-        for i in range(0, zero) : 
-            nums.append(0)
-
-# Two Pointer
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        zero = 0
-        nonzero = 0
-        while nonzero < len(nums) : 
-            if nums[nonzero] != 0 : 
-                nums[zero], nums[nonzero] = nums[nonzero], nums[zero]
-                zero += 1
-            nonzero += 1
+        insertIndex = 0
+        for i in range(0, len(nums)) : 
+            if nums[i] != 0 : 
+                nums[i], nums[insertIndex] = nums[insertIndex], nums[i]
+                insertIndex += 1

abeb2d01b58a4c0a198e30effb6036ac378ae17a
shantanuk09 <shantanuk@tutanota.com>
2022-12-24 11:00:59 -0500

    20221224

--- LeetCode/62. Unique Paths.cpp
+++ LeetCode/62. Unique Paths.cpp
@@ -1,14 +1,14 @@
 // LeetCode 62. Unique Paths
-// Dynamic Programming 
-class Solution {
+// Implementation.
+class Solution{
 public:
-    int uniquePaths(int m, int n) {
-        int arr[m + 1][n + 1];
-        memset(arr, 0, sizeof(arr));
-        arr[1][1] = 1;
+    int uniquePaths(int m, int n){
+        int memo[m + 1][n + 1];
+        memset(memo, 0, sizeof(memo));
+        memo[1][1] = 1;
         for(int i = 1; i <= m; i++)
             for(int j = 1; j <= n; j++)
-                arr[i][j] += arr[i - 1][j] + arr[i][j - 1];
-        return arr[m][n];
+                memo[i][j] += memo[i - 1][j] + memo[i][j - 1];
+        return memo[m][n];
     }
 };

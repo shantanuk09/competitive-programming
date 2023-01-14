a3a0f9cc952bd9157534ba498d2a45d0e5a7bae6
Shantanu <shantanuk@tutanota.com>
2023-01-14 13:42:27 -0500

    20230114

---
+++ LeetCode/392. Is Subsequence.py
@@ -0,0 +1,8 @@
+# LeetCode 392. Is Subsequence
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while i < len(s) and j < len(t) : 
+            if s[i] == t[j] : i += 1
+            j += 1
+        return i == len(s)

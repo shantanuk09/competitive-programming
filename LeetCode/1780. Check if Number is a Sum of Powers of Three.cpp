2903275ac177852beca551aa396e35d0fb37cf1e
Shantanu <shantanuk@tutanota.com>
2023-01-22 20:05:11 -0500

    20230122

--- LeetCode/1780. Check if Number is a Sum of Powers of Three.cpp
+++ LeetCode/1780. Check if Number is a Sum of Powers of Three.cpp
@@ -20,3 +20,15 @@ public:
         return flag;
     }
 };
+
+// Alternate.
+class Solution{
+public:
+    bool checkPowersOfThree(int n){
+        while(n > 1 && n % 3 != 2){
+            if(n % 3 == 0) n /= 3;
+            else n--;
+        }
+        return (n == 1);
+    }
+};

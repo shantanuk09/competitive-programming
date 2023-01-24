3a3f28b88c1d9f53b9d201aec1db3f75baa91e7a
Shantanu <shantanuk@tutanota.com>
2023-01-24 10:56:26 -0500

    20230124

--- LeetCode/213. House Robber II.cpp
+++ LeetCode/213. House Robber II.cpp
@@ -2,19 +2,22 @@
 // Dynamic Programming
 class Solution{
 public:
-    int robber(vector<int>& nums, int l, int r){
-        int previous = 0, current = 0;
-        for (int i = l; i <= r; i++) {
-            int temp = max(previous + nums[i], current);
-            previous = current;
-            current = temp;
-        }
-        return current;
+    int robber(vector<int> &nums, int start, int end){
+        int n = end - start + 1;
+        if(n == 2) return max(nums[start], nums[start + 1]);
+        int arr[n];
+        arr[0] = nums[start];
+        arr[1] = max(nums[start], nums[start + 1]);
+        for(int i = 2; i < n; i++)
+            arr[i] = max(arr[i - 2] + nums[start + i], arr[i - 1]);
+        return arr[n - 1];
     }
-    
+
     int rob(vector<int>& nums){
-        int n = nums.size(); 
-        if (n < 2) return n ? nums[0] : 0;
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
+        // Rob from house 0 to (n - 2) or house 1 to (n - 1).
         return max(robber(nums, 0, n - 2), robber(nums, 1, n - 1));
     }
 };

626e0e0c958c27629f0128ccf1be99d7ba91afcc
shantanuk09 <shantanuk@tutanota.com>
2023-02-22 11:21:53 -0500

    20230222

--- LeetCode/219. Contains Duplicate II.cpp
+++ LeetCode/219. Contains Duplicate II.cpp
@@ -14,3 +14,16 @@ public:
         return false;
     }
 };
+
+// Simpler.
+class Solution{
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k){
+        unordered_map<int, int> index;
+        for(int i = 0; i < nums.size(); i++){
+            if(index.find(nums[i]) != index.end() && abs(i - index[nums[i]]) <= k) return true;
+            index[nums[i]] = i; 
+        }
+        return false;
+    }
+};

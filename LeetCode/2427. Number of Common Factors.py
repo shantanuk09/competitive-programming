c8866be5ff186c747b89e74f349d63ffd243834a
shantanuk09 <shantanuk@tutanota.com>
2023-03-04 10:30:56 -0500

    20230304

---
+++ LeetCode/2427. Number of Common Factors.py
@@ -0,0 +1,7 @@
+# LeetCode 2427. Number of Common Factors
+class Solution:
+    def commonFactors(self, a: int, b: int) -> int:
+        factors = 0
+        for i in range(1, min(a, b) + 1) : 
+            if a % i == 0 and b % i == 0 : factors += 1
+        return factors
